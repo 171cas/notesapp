2b01ce6daf8537f6a5deb4211cac63939835f84d
Cesar Solano <171cas@gmail.com>
2024-08-12 21:00:22 -0500

    missing-files

---
+++ amplify/functions/say-hello/handler.ts
@@ -0,0 +1,10 @@
+import type { Schema } from "../../data/resource"
+
+export const handler: Schema["sayHello"]["functionHandler"] = async (event, context) => {
+  console.log('event', event)
+  console.log('context', context)
+  // arguments typed from `.arguments()`
+  const { name } = event.arguments
+  // return typed from `.returns()`
+  return `Hello, ${name}!`
+}
